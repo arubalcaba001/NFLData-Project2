270369155d98eccd5633be32209e2cd8849fa165
Laura Sun <sun.laura@outlook.com>
2019-04-08 17:41:29 -0700

    added tables

--- code/schema.sql
+++ code/schema.sql
@@ -15,3 +15,69 @@ CREATE TABLE team (
     PD INT(11),
     PRIMARY KEY (id)
 );
+
+CREATE TABLE Players (
+    player_id INT AUTO_INCREMENT NOT NULL,
+    First_name VARCHAR(50),
+    Last_name VARCHAR(50),
+    Team VARCHAR(150),
+    Age INT,
+    Salary INT,
+    PRIMARY KEY (player_id)
+);
+
+CREATE TABLE player_game_data (
+    player_id INT AUTO_INCREMENT NOT NULL,
+    First_name VARCHAR(50),
+    Last_name VARCHAR(50),
+    Team VARCHAR(150),
+    G INT,
+    GS INT,
+    PRIMARY KEY (player_id)
+);
+
+CREATE TABLE player_passing_data (
+    player_id INT AUTO_INCREMENT NOT NULL,
+    First_name VARCHAR(50),
+    Last_name VARCHAR(50),
+    Team VARCHAR(150),
+    Cmp INT,
+    Att INT,
+    Yds INT,
+    TD INT,
+    PRIMARY KEY (player_id)
+);
+
+CREATE TABLE player_rushing_data (
+    player_id INT AUTO_INCREMENT NOT NULL,
+    First_name VARCHAR(50),
+    Last_name VARCHAR(50),
+    Team VARCHAR(150),
+    Att INT,
+    Yds INT,
+    Y/A INT,
+    TD INT,
+    PRIMARY KEY (player_id)
+);
+
+
+CREATE TABLE player_fumbles_data (
+    player_id INT AUTO_INCREMENT NOT NULL,
+    First_name VARCHAR(50),
+    Last_name VARCHAR(50),
+    Team VARCHAR(150),
+    Fmb INT,
+    Fl INT,
+    PRIMARY KEY (player_id)
+);
+
+
+CREATE TABLE player_scoring_data (
+    player_id INT AUTO_INCREMENT NOT NULL,
+    First_name VARCHAR(50),
+    Last_name VARCHAR(50),
+    Team VARCHAR(150),
+    TD INT,
+    2PM INT,
+    PRIMARY KEY (player_id)
+);
