fe0617998e23b4497d31ecbb11fef5c8bfb9094c
Laura Sun <sun.laura@outlook.com>
2019-04-09 18:46:25 -0700

    itworks

--- Extraction.py
+++ Extraction.py
@@ -1,17 +1,25 @@
 #Import dependencies
 import pandas as pd
 
-#Extract 2018 players data
-NFLPlayer2018_Data = "Data/2018NFLPlayerStatistics.csv"
-NFLPlayer2018_df = pd.read_csv(NFLPlayer2018_Data)
+def extractit():
+    try:
+        #Extract 2018 players data
+        NFLPlayer2018_Data = "Data/2018NFLPlayerStatistics.csv"
+        NFLPlayer2018_df = pd.read_csv(NFLPlayer2018_Data)
+        NFLPlayer2018_df
+        #Extract 2018 player salaries
+        NFLPlayerSalaries = "Data/PlayerSalaries.csv"
+        PlayerSalariesdf = pd.read_csv(NFLPlayerSalaries)
+        NFLPlayer2018_df
+        #Extract team data
+        csv_file = "Data/sportsref_download_AFC_2018.csv"
+        AFC_df = pd.read_csv(csv_file)
+        NFLPlayer2018_df
+        csv_file = "Data/sportsref_download_NFC_2018.csv"
+        NFC_df = pd.read_csv(csv_file)
+        NFLPlayer2018_df
+        print("Completed")
+    except:
+        print("Failed")
 
-#Extract 2018 player salaries
-NFLPlayerSalaries = "Data/PlayerSalaries.csv"
-PlayerSalariesdf = pd.read_csv(NFLPlayerSalaries)
-
-#Extract team data
-csv_file = "../team/sportsref_download_AFC_2018.csv"
-AFC_df = pd.read_csv(csv_file)
-
-csv_file = "../team/sportsref_download_NFC_2018.csv"
-NFC_df = pd.read_csv(csv_file)
+extractit()
