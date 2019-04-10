d050e23b19c1240fdf4bd6d0b5ae338b85eb78d7
Laura Sun <sun.laura@outlook.com>
2019-04-09 20:15:05 -0700

    not functional

---
+++ loader.py
@@ -0,0 +1,23 @@
+###################### Loading #######################
+# Create a database connection
+
+def loadzone():
+    try:
+        connection_name = "root:root@127.0.0.1/nfl_db"
+        engine = create_engine(f'mysql+pymysql://{connection_name}')
+
+# #Confirm tables
+# def confirm():
+#     return engine.table_names()
+
+# Load dataframes into DATABASE
+        player_data.to_sql(name='player_info', con=engine, if_exists='append', index=True)
+        player_game_data.to_sql(name='player_game', con=engine, if_exists='append', index=True)
+        player_passing_data.to_sql(name='player_passing', con=engine, if_exists='append', index=True)
+        player_rushing_data.to_sql(name='player_rushing', con=engine, if_exists='append', index=True)
+        player_receiving_data.to_sql(name='player_receiving', con=engine, if_exists='append', index=True)
+        player_fumbles_data.to_sql(name='player_fumbles', con=engine, if_exists='append', index=True)
+        player_scoring_data.to_sql(name='player_scoring', con=engine, if_exists='append', index=True)
+        print("Completed")
+    except:
+        print("Failed")
