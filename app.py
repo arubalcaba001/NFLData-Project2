aad8eada498d3c6065ce3a1875ff4d941d4035f6
Laura Sun <sun.laura@outlook.com>
2019-04-09 21:23:26 -0700

    updatedapp

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-import pandas as pd
-from sqlalchemy import create_engine
 import extraction
 import transformation
 import loader
 
 
-if __name__ == '__main__':
+def main():
     df1 = extraction.extract_csv("Data/2018NFLPlayerStatistics.csv")
     df2 = extraction.extract_csv("Data/PlayerSalaries.csv")
     df3 = extraction.extract_csv("Data/sportsref_download_AFC_2018.csv")
     df4 = extraction.extract_csv("Data/sportsref_download_NFC_2018.csv")
     transformation.clean_playerstats(df1)
+    transformation.playersalaries(df2)
+    tranformation.teamdata(df3,df4)
     # loader.loadzone()
     # print("Done!")
+
+if __name__ == '__main__':
+    main()
